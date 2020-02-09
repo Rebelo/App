8dc441199fa5fba45f500decc95d909c7819a173
Leonardo da Silva Rebelo <leonardorebelo@outlook.com>
2020-02-09 06:29:14 -0300

    again

--- controllers/plans-controller.test.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-describe('chai test', ()=>{
-    it('should compare some values', ()=>{
-        expect(1).to.equal(1);
-    })
-
-})
--- utilities/async-wrapper.d.ts
+++
@@ -1,5 +0,0 @@
-//we say that the callback is a requestHandler
-
-import {RequestHandler} from 'express';
-
-export function AsyncWrapper(fn: RequestHandler);
